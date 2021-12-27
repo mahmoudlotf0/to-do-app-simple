b1e090495611b9ffd7f600b590caf047a986ff05
mahmoudlotf0 <lotfymahmoud07@outlook.com>
2021-12-28 00:41:10 +0200

    Add no tasks widget

---
+++ lib/ui/widgets/no_tasks_yet.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class NoTasksYet extends StatelessWidget {
+  const NoTasksYet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Icon(
+            Icons.menu,
+            size: 100,
+            color: Colors.grey,
+          ),
+          Text(
+            'No tasks yet, please add some tasks',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
